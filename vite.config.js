8f4a8580f2771b832ca62ce82564308125731db5
Bahodir <109802068+bobov1190@users.noreply.github.com>
2025-07-19 15:52:59 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+export default {
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173
+  }
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
